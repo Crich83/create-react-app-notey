f7873d94d310367a701934e27b42cb9cf2c8a964
Crich83 <57454470+Crich83@users.noreply.github.com>
2020-08-05 20:43:01 -0400

    Add files via upload

---
+++ precache-manifest.dfa6a4f7da29ea72c7f61c5b7332785d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f140d3caedf0a327996d138cbebb4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e6a7d787df3e82157c",
+    "url": "/static/css/main.671d4391.chunk.css"
+  },
+  {
+    "revision": "7650010653b7b79f26bc",
+    "url": "/static/js/2.336686d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.336686d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e6a7d787df3e82157c",
+    "url": "/static/js/main.84817806.chunk.js"
+  },
+  {
+    "revision": "9798ffa36ca012fa0852",
+    "url": "/static/js/runtime-main.26a45552.js"
+  }
+]);
